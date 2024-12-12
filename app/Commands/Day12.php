ead24514ba0d6522e73ca4dbc88c9c8959b488e3
Eugene Mah <eugenemah@gmail.com>
2024-12-12 05:19:15 -0500

    New command for AoC Day 12

---
+++ app/Commands/Day12.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Commands;
+
+use LaravelZero\Framework\Commands\Command;
+
+class Day12 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:day12 {data}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Advent of Code 2024 Day 12';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // Garden fences
+        // Calculate the area and perimeter of each garden
+        // plot identified by a letter.
+    }
+}
