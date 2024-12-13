94fad72237a62546f4d08802d1d91d689be36481
Eugene Mah <eugenemah@gmail.com>
2024-12-13 06:23:32 -0500

    Load data and start parsing

--- app/Commands/Day13.php
+++ app/Commands/Day13.php
@@ -2,8 +2,8 @@
 
 namespace App\Commands;
 
-use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
+use MCordingley\LinearAlgebra\Matrix;
 
 class Day13 extends Command
 {
@@ -12,28 +12,36 @@ class Day13 extends Command
      *
      * @var string
      */
-    protected $signature = 'app:day13';
+    protected $signature = 'app:day13 {data}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Advent of Code 2024 Day 13';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
-    }
+        // Claw game
+        $puzzle = file($this->argument('data'), FILE_IGNORE_NEW_LINES);
 
-    /**
-     * Define the command's schedule.
-     */
-    public function schedule(Schedule $schedule): void
-    {
-        // $schedule->command(static::class)->everyMinute();
+        // Each group in the puzzle input is three lines
+        // First line gives number of units the claw will move when A is pushed
+        // Second line gives number of units the claw will move when B is pushed
+        // Third line gives the prize location.
+        // Groups are separated by newlines.
+
+        // Use a matrix math approach
+        // M = [[Ax Bx]  Movement A and B buttons
+        //      [Ay By]]
+        // P = [Px Py] prize position
+        // M*N = P where N is the number of button presses for A and B
+        // N = inv(M)*P
+        // so N can be calculated by determining the inverse of M and then
+        // matrix multiplication with P
     }
 }
