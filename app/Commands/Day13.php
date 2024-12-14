d8e516b57858c7823e0174544a1ce317fe1a5088
Eugene Mah <eugenemah@gmail.com>
2024-12-13 19:44:16 -0500

    Works on test data, not giving the right answer on full input data

--- app/Commands/Day13.php
+++ app/Commands/Day13.php
@@ -43,5 +43,47 @@ class Day13 extends Command
         // N = inv(M)*P
         // so N can be calculated by determining the inverse of M and then
         // matrix multiplication with P
+
+        $tokens = 0;
+        $nPrizes = 0;
+        while (count($puzzle) > 0) {
+            // Get Button A line
+            $a = array_shift($puzzle);
+            // Tack on the Button B line
+            $a .= array_shift($puzzle);
+            // Get Prize line
+            $prize = array_shift($puzzle);
+            // Skip the blank line
+            array_shift($puzzle);
+
+            // Get the Button A and B movements
+            $match[] = preg_match_all('/\d+/', $a, $match);
+            $ax = intval($match[0][0]); // Ax
+            $ay = intval($match[0][1]); // Ay
+            $bx = intval($match[0][2]); // Bx
+            $by = intval($match[0][3]); // By
+
+            // If the determinant of the matrix != 0, the
+            // matrix can be inverted and there's a solution
+            $det = ($ax*$by) - ($bx*$ay);
+            $this->info("Determinant: ".$det);
+            if ($det != 0) {
+                // Get the prize coordinates
+                $match[] = preg_match_all('/\d+/', $prize, $match);
+                $px = intval($match[0][0]); // Px
+                $py = intval($match[0][1]); // Py
+                // Calculate the number of A and B button presses
+                $x = intval(($px*$by - $py*$bx)/$det); // A button presses
+                $y = intval((-$px*$ay + $py*$ax)/$det); // B button presses
+
+                $this->info($x." ".$y);
+                if (($x > 0 && $x <= 100) && ($y > 0 && $y <= 100)) {
+                    $nPrizes++; // Won a prize!
+                    $tokens += $x*3 + $y;
+                }
+            }
+        }
+        $this->info("Won " . $nPrizes . " prizes!");
+        $this->info("Had to spend " . $tokens . " to win the prizes.");
     }
 }
